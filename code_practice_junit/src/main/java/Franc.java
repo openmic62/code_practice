74e2ce9efd845eeadb5ffa3ae51a2373f4b2d53c
Mike Rocha <michaelr@vifla.com>
2013-10-11 18:21:43 +0000

    michaelr - TDD, p. 47a; books says to back out of the change that broke the tests. Ok, but I'm saving here first so I can go back if I want to.

--- code_practice_junit/src/main/java/Franc.java
+++ code_practice_junit/src/main/java/Franc.java
@@ -10,7 +10,7 @@ public class Franc extends Money{
 		super(amount, currency);
 	}
 	Money times(int multiplier){
-		return new Franc(amount * multiplier, currency);
+		return new Money(amount * multiplier, currency);
 		///return new Franc(amount * multiplier, "CHF");
 		////return Money.franc(amount * multiplier);
 	}
