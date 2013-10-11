81fca0f6d8dcd7b023380b2aaf91d0af3ed5b4db
michaelr <michaelr@50d94a75-6656-bf47-8487-448895b3180b>
2013-10-11 16:50:19 +0000

    michaelr - TDD, p. 42b; finished moving the currency strings to the factory method; almost done with abstracting away the subclasses.
    git-svn-id: svn://localhost/code_practice/trunk@28 50d94a75-6656-bf47-8487-448895b3180b

--- code_practice_junit/src/main/java/Money.java
+++ code_practice_junit/src/main/java/Money.java
@@ -1,8 +1,12 @@
 public abstract class Money {
 	protected int amount;
+	protected String currency;
 
-	abstract String currency();
 	abstract Money times(int multiplier);
+	
+	String currency() {
+		return currency;
+	}
 	
 	public boolean equals(Object object) {
 		Money money = (Money) object;
@@ -11,10 +15,10 @@ public abstract class Money {
 	}
 	
 	static Money dollar(int amount) {
-		return new Dollar(amount);
+		return new Dollar(amount, "USD");
 	}
 	
 	static Money franc(int amount) {
-		return new Franc(amount);
+		return new Franc(amount, "CHF");
 	}
 }
