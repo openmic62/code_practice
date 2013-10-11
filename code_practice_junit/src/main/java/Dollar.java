544581ef0363f2726b036e535e4287d572645bf8
Mike Rocha <michaelr@vifla.com>
2013-10-11 17:06:28 +0000

    michaelr - TCC, p. 43a; finished instituting the notion of currency.

--- code_practice_junit/src/main/java/Dollar.java
+++ code_practice_junit/src/main/java/Dollar.java
@@ -7,13 +7,9 @@ public class Dollar extends Money {
 	private String currency;
 	
 	Dollar(int amount, String currency){
-		this.amount = amount;
-		this.currency = currency;
+		super(amount, currency);
 	}
 	Money times(int multiplier){
 		return Money.dollar(amount * multiplier);
-	}
-	String currency() {
-		return currency;
 	}
 }
--- code_practice_junit/src/main/java/Franc.java
+++ code_practice_junit/src/main/java/Franc.java
@@ -7,13 +7,9 @@ public class Franc extends Money{
 	private String currency;
 	
 	Franc(int amount, String currency){
-		this.amount = amount;
-		this.currency = currency;
+		super(amount, currency);
 	}
 	Money times(int multiplier){
 		return Money.franc(amount * multiplier);
-	}
-	String currency() {
-		return currency;
 	}
 }
