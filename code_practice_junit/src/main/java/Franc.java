589f111d8f347c0a0330a4f054a8feb3604504b9
Mike Rocha <michaelr@vifla.com>
2013-10-11 13:05:13 +0000

    michaelr - TDD p.32b at the end of Ch. 6 "Equality for All, Redux".

--- code_practice_junit/src/main/java/Franc.java
+++ code_practice_junit/src/main/java/Franc.java
@@ -3,16 +3,11 @@ H:\student\code_practice_junit\src\test\java>javac -cp ..\..\..\lib\junit-4.11.j
 H:\student\code_practice_junit\src\test\java>java -cp ..\..\..\lib\junit-4.11.jar;..\..\..\lib\hamcrest-core-1.3.jar;..\..\..\target\classes org.junit.runner.JUnitCore MoneyTests
 */
 
-public class Franc {
-	private int amount;
+public class Franc extends Money{
 	Franc(int amount){
 		this.amount = amount;
 	}
 	Franc times(int multiplier){
 		return new Franc(amount * multiplier);
-	}
-	public boolean equals(Object object) {
-		Franc franc = (Franc) object;
-		return this.amount == franc.amount;
 	}
 }
--- code_practice_junit/src/test/java/MoneyTests.java
+++ code_practice_junit/src/test/java/MoneyTests.java
@@ -33,4 +33,10 @@ public class MoneyTests {
 		assertTrue(new Dollar(5).equals(new Dollar(5)));
 		assertFalse(new Dollar(5).equals(new Dollar(6)));
 	}
+	
+	@Test
+	public void testFrancEquality(){
+		assertTrue(new Franc(5).equals(new Franc(5)));
+		assertFalse(new Franc(5).equals(new Franc(6)));
+	}
 }
