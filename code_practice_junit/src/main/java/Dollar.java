ed381a84c32451ced9d9f5b9a0b52046ccac6b1a
michaelr <michaelr@50d94a75-6656-bf47-8487-448895b3180b>
2013-10-11 13:05:13 +0000

    michaelr - TDD p.32b at the end of Ch. 6 "Equality for All, Redux".
    git-svn-id: svn://localhost/code_practice/trunk@18 50d94a75-6656-bf47-8487-448895b3180b

--- code_practice_junit/src/main/java/Dollar.java
+++ code_practice_junit/src/main/java/Dollar.java
@@ -3,16 +3,11 @@ H:\student\code_practice_junit\src\test\java>javac -cp ..\..\..\lib\junit-4.11.j
 H:\student\code_practice_junit\src\test\java>java -cp ..\..\..\lib\junit-4.11.jar;..\..\..\lib\hamcrest-core-1.3.jar;..\..\..\target\classes org.junit.runner.JUnitCore MoneyTests
 */
 
-public class Dollar {
-	private int amount;
+public class Dollar extends Money {
 	Dollar(int amount){
 		this.amount = amount;
 	}
 	Dollar times(int multiplier){
 		return new Dollar(amount * multiplier);
-	}
-	public boolean equals(Object object) {
-		Dollar dollar = (Dollar) object;
-		return this.amount == dollar.amount;
 	}
 }
