279f28a6bffbe68a7efc8b76edc37b592a160168
michaelr <michaelr@50d94a75-6656-bf47-8487-448895b3180b>
2013-10-11 19:50:34 +0000

    michaelr - TDD, p. 51b; Cleaned up the code. Removed commented out stuff. Ready to go for Ch. 11 "The Root of All Evil"
    git-svn-id: svn://localhost/code_practice/trunk@35 50d94a75-6656-bf47-8487-448895b3180b

--- code_practice_junit/src/main/java/Franc.java
+++ code_practice_junit/src/main/java/Franc.java
@@ -1,17 +1,9 @@
-/*
-H:\student\code_practice_junit\src\test\java>javac -cp ..\..\..\lib\junit-4.11.jar -d ..\..\..\target\classes MoneyTests.java
-H:\student\code_practice_junit\src\test\java>java -cp ..\..\..\lib\junit-4.11.jar;..\..\..\lib\hamcrest-core-1.3.jar;..\..\..\target\classes org.junit.runner.JUnitCore MoneyTests
-*/
-
 public class Franc extends Money{
-	//private String currency;
 	
 	Franc(int amount, String currency){
 		super(amount, currency);
 	}
 	Money times(int multiplier){
 		return new Money(amount * multiplier, currency);
-		///return new Franc(amount * multiplier, "CHF");
-		////return Money.franc(amount * multiplier);
 	}
 }
