ff66a90cb2127b442087b686f2793f618b4dfd75
michaelr <michaelr@50d94a75-6656-bf47-8487-448895b3180b>
2013-10-11 14:33:19 +0000

    michaelr - TDD, p. 35(all over it) - 36a. The book made a seemingly simple change for the return values of the Fran.times(int) method. Before: Franc times(int multiplier) {...} After:  Money times(int multiplier) {...}
    This caused 
    runtest:
        [junit] Running MoneyTests
        [junit] Testsuite: MoneyTests
        [junit] Tests run: 5, Failures: 0, Errors: 1, Skipped: 0, Time elapsed: 0.242 sec
        [junit] Tests run: 5, Failures: 0, Errors: 1, Skipped: 0, Time elapsed: 0.242 sec
        [junit]
        [junit] Testcase: testDollarMultiplication took 0.015 sec
        [junit] Testcase: testFrancEquality took 0.003 sec
        [junit] Testcase: testDollarFrancInequality took 0.001 sec
        [junit] Testcase: testDollarEquality took 0 sec
        [junit] Testcase: testFrancMultiplication took 0.004 sec
        [junit]     Caused an ERROR
        [junit] Franc.times(I)LFranc;
        [junit] java.lang.NoSuchMethodError: Franc.times(I)LFranc;
        [junit]
        [junit] Test MoneyTests FAILED
    
    RCA on p.35 shows the test failure came from assertEquals(expectedObj, actualObj),
    implemented as 
    		Franc five = new Franc(5);
    		assertEquals(new Franc(10), five.times(2));
    Specifically, if I expect a Franc, but get a Money, then I fail.
    (A Money is not always a Franc)
    
    If I swap the expected and actual args in the implementation like this ...
    		Franc five = new Franc(5);
    		assertEquals(five.times(2), new Franc(10));
    		///assertEquals(new Franc(10), five.times(2));
    ... then the test passes because ...
        if I expect a Money, but get a Franc, then I pass.
        (A Franc is ALWAYS a Money.)
    
    
    
    git-svn-id: svn://localhost/code_practice/trunk@20 50d94a75-6656-bf47-8487-448895b3180b

--- code_practice_junit/src/test/java/MoneyTests.java
+++ code_practice_junit/src/test/java/MoneyTests.java
@@ -24,8 +24,9 @@ public class MoneyTests {
 	@Test
 	public void testFrancMultiplication() {
 		Franc five = new Franc(5);
-		assertEquals(new Franc(10), five.times(2));
-		assertEquals(new Franc(15), five.times(3));
+		assertEquals(five.times(2), new Franc(10));
+		///assertEquals(new Franc(10), five.times(2));
+		///assertEquals(new Franc(15), five.times(3));
 	}
 	
 	@Test
