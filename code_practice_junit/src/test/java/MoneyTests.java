1811b0f175bf36a61d394b1868083a9edea69980
Mike Rocha <michaelr@vifla.com>
2013-10-11 13:32:01 +0000

    michaelr - TDD p.34b at the end of Ch. 7 "Apples and Oranges".

--- code_practice_junit/src/test/java/MoneyTests.java
+++ code_practice_junit/src/test/java/MoneyTests.java
@@ -39,4 +39,10 @@ public class MoneyTests {
 		assertTrue(new Franc(5).equals(new Franc(5)));
 		assertFalse(new Franc(5).equals(new Franc(6)));
 	}
+	
+	@Test
+	public void testDollarFrancInequality(){
+		assertFalse(new Dollar(5).equals(new Franc(5)));
+		assertFalse(new Franc(5).equals(new Dollar(5)));
+	}
 }
