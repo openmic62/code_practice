e5441b1fde755f6b9371d1024078b5945d9db4c1
Mike Rocha <openmic62@gmail.com>
2013-10-12 02:51:47 +0000

    michaelr - TDD, p. 58a; constructed testSimpleAddition using the ideas of Expressions and Bank.

--- code_practice_junit/src/test/java/MoneyTests.java
+++ code_practice_junit/src/test/java/MoneyTests.java
@@ -12,8 +12,11 @@ public class MoneyTests {
 
 	@Test
 	public void testSimpleAddition() {
-		Money sum = Money.dollar(5).plus(Money.dollar(5));
-		assertEquals(Money.dollar(10), sum);
+		Money five = Money.dollar(5);
+		Money sum = five.plus(five);
+		Bank bank = new Bank();
+		Money reduced = bank.reduce(sum, "USD");
+		assertEquals(Money.dollar(10), reduced);
 	}
 	
 	@Test
