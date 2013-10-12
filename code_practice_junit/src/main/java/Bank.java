4ca253bc3275bf0bec4dc27c9e5552ba820790be
Mike <Mike@50d94a75-6656-bf47-8487-448895b3180b>
2013-10-12 23:34:21 +0000

    michaelr - TDD, p. 69b; created Pair private class with overrides for equals() and hashCode().
    git-svn-id: svn://localhost/code_practice/trunk@53 50d94a75-6656-bf47-8487-448895b3180b

--- code_practice_junit/src/main/java/Bank.java
+++ code_practice_junit/src/main/java/Bank.java
@@ -12,4 +12,23 @@ class Bank {
 
 	// <mlr 131012: TDD, p. 67b; had to add this to get the green bar mentioned in the book.>
 	void addRate(String currency1, String currency2, int exchangeRate) {}
+	
+	private class Pair {
+		private String from;
+		private String to;
+		
+		Pair( String from, String to ) {
+			this.from = from;
+			this.to = to;
+		}
+		
+		public boolean equals( Object obj ) {
+			Pair pair = (Pair) obj;
+			return from.equals(pair.from) && to.equals(pair.to);
+		}
+		
+		public int hashCode() {
+			return 0;
+		}
+	}
 }
