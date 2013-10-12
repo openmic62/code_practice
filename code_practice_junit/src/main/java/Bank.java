2dd6184471ab5758e6b58fbe4229cc87f01e42d9
Mike <Mike@50d94a75-6656-bf47-8487-448895b3180b>
2013-10-12 19:44:57 +0000

    michaelr - TDD, p. 64b; "Ok. Whatever ... I'm not following this.)
    git-svn-id: svn://localhost/code_practice/trunk@48 50d94a75-6656-bf47-8487-448895b3180b

--- code_practice_junit/src/main/java/Bank.java
+++ code_practice_junit/src/main/java/Bank.java
@@ -1,5 +1,6 @@
 class Bank {
 	Money reduce(Expression sourceExp4Money, String to) {
+		if (sourceExp4Money instanceof Money) return (Money) sourceExp4Money;
 		Sum sum = (Sum) sourceExp4Money;
 		return sum.reduce(to);
 		//return Money.dollar(10);
