afc88ef296399f7fc9077594c075f922091a992f
Mike <Mike@50d94a75-6656-bf47-8487-448895b3180b>
2013-10-12 23:21:14 +0000

    michaerl - TDD, p. 67b2; moved the rate calculation to Bank from Money; changed money to ask Bank for the rate.
    git-svn-id: svn://localhost/code_practice/trunk@52 50d94a75-6656-bf47-8487-448895b3180b

--- code_practice_junit/src/main/java/Bank.java
+++ code_practice_junit/src/main/java/Bank.java
@@ -3,7 +3,13 @@ class Bank {
 		return source.reduce(this, to);
 		//return source.reduce(to);
 	}
-	
+
+	int rate(String from, String to) {
+		return (from.equals("CHF") && to.equals("USD"))
+		? 2
+		: 1;
+	}
+
 	// <mlr 131012: TDD, p. 67b; had to add this to get the green bar mentioned in the book.>
 	void addRate(String currency1, String currency2, int exchangeRate) {}
 }
