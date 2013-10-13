a1d52ed45a4c7779b7c3d2f3c167530cc29b89cd
Mike Rocha <openmic62@gmail.com>
2013-10-13 02:19:58 +0000

    michaelr - TDD, p. 77b; wrote testSumPlusMoney; implemented Sum.plus. All code compiles and all tests pass.
    This seems to me VERY elegant. It seems recursive, but I'm not sure if it is, when I look at Sum.reduce in the context of how testSumPlusMoney executes. I should spend some time figuring it out.

--- code_practice_junit/src/test/java/MoneyTests.java
+++ code_practice_junit/src/test/java/MoneyTests.java
@@ -11,6 +11,18 @@ import org.junit.runners.JUnit4;
 public class MoneyTests {
 	
 	@Test
+	public void testSumPlusMoney() {
+		Expression fiveBucks = Money.dollar(5);
+		Expression tenFrancs  = Money.franc(10);
+		Bank bank = new Bank();
+		bank.addRate("CHF", "USD", 2);
+		Expression sum = new Sum(fiveBucks, tenFrancs).plus(fiveBucks);
+		Money result = bank.reduce(sum, "USD");
+		assertEquals(Money.dollar(15), result);
+	}
+		
+	
+	@Test
 	public void testMixedAddition() {
 		Expression fiveBucks = Money.dollar(5);
 		Expression tenFrancs  = Money.franc(10);
