90f22f0a6a83a09f17cb7a1f0251fd0375e1842c
Mike Rocha <michaelr@vifla.com>
2013-10-18 17:00:15 +0000

    michaelr - GOOS; p. 85b: made a good start - added group "Auction Sniper" to the UE project."code_practice_junit_ue_project.prj". - created src and test sub-groups - created acceptance, integration, and unit sub-groups under the test sub-group. - saved MoneyTests.java as AuctionSniperEndToEndTests.java - made one empty test in the test class and ran it with success!!!   H:\student\code_practice_junit>ant runtest -DtestClass=AuctionSniperEndToEndTests

---
+++ code_practice_junit/src/test/java/AuctionSniperEndToEndTests.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class AuctionSniperEndToEndTests {
+	
+	@Test
+	public void testSumTimes() {
+	}		
+}
