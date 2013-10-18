0ef4745f9f263a0403f0c5331eeb114bb1a4e62a
Mike Rocha <michaelr@vifla.com>
2013-10-18 19:25:54 +0000

    michaelr - GOOS, p. 85b1; input the body of the Auction Sniper walking skeleton. - Tried to write it on my own first. - Then saw if I got pretty close. I did! :-)

--- code_practice_junit/src/test/java/AuctionSniperEndToEndTests.java
+++ code_practice_junit/src/test/java/AuctionSniperEndToEndTests.java
@@ -2,6 +2,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.After;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,7 +11,26 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class AuctionSniperEndToEndTests {
 	
+	private final FakeAuctionServer auction = new FakeAuctionServer("item-54321");
+	private final ApplicationRunner application = new ApplicationRunner();
+	
 	@Test
-	public void testSumTimes() {
-	}		
+	public void sniperJoinsAuctionUntilAuctionCloses() throws Exception {        
+		auction.startSellingItem();                    // step 1
+		application.startBiddingIn(auction);           // step 2
+		auction.hasReceivedJoinRequestFromSniper();    // step 3
+		auction.announceClosed();                      // step 4
+		application.showsSniperHasLostAuction();       // step 5
+	}
+	
+	// clean up
+	@After
+	public void stopAuction() {
+		auction.stop();
+	}
+	
+	@After
+	public void stopApplication() {
+		application.stop();
+	}
 }
