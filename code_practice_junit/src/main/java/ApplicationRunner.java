0e034b462d118b49ca3b25fc6a57dc6a702e689f
Mike Rocha <openmic62@gmail.com>
2013-10-19 00:49:21 +0000

    michaelr - GOOS, p.88a; I did what I learned in the TDD book ... get the test compiling and running as fast as possible. Even if it fails, that is good. Well, my test actually passed because I stubbed everything and the end-to-end test contains no asserts.
    Anyway, GOOS doesn't do what I did, but I don't care. I feel good.

---
+++ code_practice_junit/src/main/java/ApplicationRunner.java
@@ -0,0 +1,8 @@
+public class ApplicationRunner {
+	
+	public void startBiddingIn(FakeAuctionServer auction) {}
+	
+	public void showsSniperHasLostAuction() {}
+	
+	public void stop() {}
+}
---
+++ code_practice_junit/src/main/java/FakeAuctionServer.java
@@ -0,0 +1,12 @@
+public class FakeAuctionServer {
+	FakeAuctionServer(String item) {
+	}
+	
+	public void startSellingItem() {}
+	
+	public void hasReceivedJoinRequestFromSniper() {}
+	
+	public void announceClosed() {}
+	
+	public void stop() {}
+}
