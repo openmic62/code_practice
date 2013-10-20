1e759a3fe72925ad6da7e7e3f94a05b2bd65e07c
Mike <Mike@50d94a75-6656-bf47-8487-448895b3180b>
2013-10-20 22:02:25 +0000

    michaelr - GOOS, p. 91b2; - WL, build.xml, added <link to hamcrest javadocs> in target javadocs - made my ApplicationRunner look like the book's on p. 90 - made my AuctionSniperDriver look like the book's on p. 91
    This fails compile with one error.
        [javac] H:\student\code_practice_junit\src\main\java\AuctionSniperDriver.java:21: error: cannot find symbol
        [javac]                     this, named(Main.SNIPER_STATUS_NAME)).hasText(equalTo(statusText));
        [javac]                                                                   ^
        [javac]   symbol:   method equalTo(String)
        [javac]   location: class AuctionSniperDriver
    
    git-svn-id: svn://localhost/code_practice/trunk@71 50d94a75-6656-bf47-8487-448895b3180b

---
+++ code_practice_junit/src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+	
+	public static final String SNIPER_STATUS_NAME = "status";
+	public static final String STATUS_JOINING = "Joining auction";
+	public static final String STATUS_LOST = "Lost auction";
+	
+	public static void main(String[] args) {}
+}
