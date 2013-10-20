8f69419a7eb779634ed6c9db785929bd251ea8ea
Mike Rocha <openmic62@gmail.com>
2013-10-20 18:54:24 +0000

    michaelr - GOOS, p.90b; test sniperJoinsAuctionUntilAuctionCloses in class AuctionSniperEndToEndTests succeeds BEFORE adding the driver.dispose() conditional in ApplicationRunner.stop().
    BEFORE:
    	public void stop() {
    	}
    AFTER:
    	public void stop() {
    		if (driver != null) {
    		 	driver.dispose();
    		}
    	}

--- code_practice_junit/src/main/java/ApplicationRunner.java
+++ code_practice_junit/src/main/java/ApplicationRunner.java
@@ -1,8 +1,30 @@
 public class ApplicationRunner {
 	
-	public void startBiddingIn(FakeAuctionServer auction) {}
+	static final String SNIPER_ID = "sniper";
+	static final String SNIPER_PASSWORD = "sniper";
+	private AuctionSniperDriver driver;
 	
-	public void showsSniperHasLostAuction() {}
+	public void startBiddingIn(FakeAuctionServer auction) {
+		Thread thread = new Thread("Test Application") {
+			@Override public void run() {
+				final String[] mainArgs = {SNIPER_ID, SNIPER_PASSWORD};
+				try {
+					Main.main(mainArgs);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		};
+		thread.setDaemon(true);
+		thread.start();
+		driver = new AuctionSniperDriver(1000);
+		driver.showSniperStatus(Main.STATUS_JOINING);
+	}
 	
-	public void stop() {}
+	public void showsSniperHasLostAuction() {
+		driver.showSniperStatus(Main.STATUS_LOST);
+	}
+	
+	public void stop() {
+	}
 }
