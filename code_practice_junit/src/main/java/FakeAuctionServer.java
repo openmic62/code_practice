f563fec9c92a92d3753f0a0a6b3b138e3af13d88
Mike <Mike@50d94a75-6656-bf47-8487-448895b3180b>
2013-10-21 01:16:02 +0000

    michaelr - GOOS, p. 92b; implemented FakeAuctionServer on my own the best I could using the description in GOOS, the Smack javadocs, and the online tutorial I found at ... http://javaupdates.wordpress.com/2012/03/13/how-to-write-a-simple-xmpp-jabber-client-using-the-smack-api/
    git-svn-id: svn://localhost/code_practice/trunk@72 50d94a75-6656-bf47-8487-448895b3180b

--- code_practice_junit/src/main/java/FakeAuctionServer.java
+++ code_practice_junit/src/main/java/FakeAuctionServer.java
@@ -1,8 +1,35 @@
-public class FakeAuctionServer {
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.ChatManager;
+import org.jivesoftware.smack.ChatManagerListener;
+import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.MessageListener;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.packet.Message;
+
+public class FakeAuctionServer implements ChatManagerListener, MessageListener {
+	
+	private final String item;
+	
+	private ChatManager chatManager;
+	private ConnectionConfiguration config;
+	private XMPPConnection connection;
+	
 	FakeAuctionServer(String item) {
+		this.item = item;
+		this.config = new ConnectionConfiguration("roco-3", 9090, "Southabee's");
+		this.connection = new XMPPConnection(config);
 	}
 	
-	public void startSellingItem() {}
+	public void chatCreated(Chat chat, boolean createdLocally) {}
+	
+	public void processMessage(Chat chat, Message message) {}
+	
+	public void startSellingItem() {
+		connection.connect();
+		connection.login(item, "sniper");
+		chatManager = connection.getChatManager();
+		chatManager.addChatListener(this);
+	}
 	
 	public void hasReceivedJoinRequestFromSniper() {}
 	
