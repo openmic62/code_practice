627c820dd24748185f74a7bdc9b35ba58361161e
Mike <Mike@50d94a75-6656-bf47-8487-448895b3180b>
2013-11-03 11:52:00 +0000

    michaelr - JUnitParams; I finished servicing the JUnitParams induced interrupt.
    I'm working on GOOS, p. 93 trying to write my own version of the code before I look at the book's version. I wrote a helper method "getSniperCommandFromMessage" that parses out the command from an XMPP message. While writing that method, I saw an opportunity to apply what I learned in "The Art of Unit Testing". So, I wrote 2 tests that ended up being very redundant, the only difference being the command "BID" or "JOIN". 
    
    I had a to do to gain proficiency in parameterized unit testing. So, I parameterized the JUnit way (see FakeAuctionServerPTests.java) which sucks. I found JUnitParams, which looked very promising. While implementing JUParams, I encountered a NPE when running my test from Ant. I looked up the error and found a fix at the JUnitParams code.google.com bug tracker. And that is where I spent the time servicing the interrupt.
    
    I ...
    - got my Maven build capability working.
    - saw how to take a home grown project and migrate it into the Maven central repo (from the JUnitParams code itself by pl.pragmatists).
    - submitted my own bug record (1st one!!!) to an open source project
    - gained significant confidence in my skills.
    
    It was worth the effort to service this interrupt. And now, back to our regularly scheduled program.
    
    git-svn-id: svn://localhost/code_practice/trunk@84 50d94a75-6656-bf47-8487-448895b3180b

--- code_practice_junit/src/test/java/FakeAuctionServerTests.java
+++ code_practice_junit/src/test/java/FakeAuctionServerTests.java
@@ -12,35 +12,52 @@ import org.junit.runner.RunWith;
 
 @RunWith(JUnitParamsRunner.class)
 public class FakeAuctionServerTests {
-	
-
-    @Test
-    @Parameters({ "1", "2" })
-    public void singleParam(int number) {
-        assertTrue(number > 0);
-    }
-
-    @Test
-    @Parameters({ "a \n b", "a(asdf)", "a \r a" })
-    public void specialCharsInParam(String a) throws Exception {
-    	assertEquals("a \n b", a);
-    }
-
-    @Test
-    @Parameters({ "1, false", "2, true" })
-    public void multipleParams(int number, boolean what) throws Exception {
-        assertEquals(what, number > 1);
-    }
-
-    @Test
-    @Parameters({ "1," })
-    public void emptyParam(int number, String empty) {
-        assertEquals(1, number);
-        assertEquals("", empty);
-    }
-
+/*
+  // <mlr 131031: this test passes in ant and in mvn>
 	@Test
-	public void getSniperCommandFromMessage_withCmdJOIN_returnsJOIN() {
+	@Parameters({"AAA,1", "BBB,2"})
+	public void params_in_annotation(String p1, Integer p2) { }
+	
+  // <mlr 131031: this test passes in ant and in mvn>
+  @Test
+  @Parameters({ "1", "2" })
+  public void singleParam(int number) {
+      assertTrue(number > 0);
+  }
+*/	
+/*  
+	// <mlr 131031: this test NPEs in ant, FAILS 2 tests in mvn> <<<<<<<<<<<<<<<<<<<<<<<!!!!!!!!!<<<<<<<<<<<<<<<<<<<<<<<<<<<
+  @Test
+  @Parameters({ "a \n b", "a(asdf)", "a \r a" })
+  public void specialCharsInParam(String a) throws Exception {
+  	assertEquals("a \n b", a);
+  }
+*/	
+
+/*
+  // <mlr 131031: this test passes in ant and in mvn>
+  @Test
+  @Parameters({ "1, false", "2, true" })
+  public void multipleParams(int number, boolean what) throws Exception {
+      assertEquals(what, number > 1);
+  }
+
+*/
+/*
+	// <mlr 131031: this test NPEs in ant BUT PASSES in mvn> <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+  @Test
+  @Parameters({ "1," })
+  public void emptyParam(int number, String empty) {
+      assertEquals(1, number);
+      assertEquals("", empty);
+  }
+*/
+
+  /*********************************** BEGIN - use junitparams to parameterize these 2 tests */
+  /*
+  // <mlr 131031: this test passes in ant and in mvn>
+	@Test
+	public void getSniperCommandFromMessage_withCmdJOIN_returnsJOINString() {
 		
 		FakeAuctionServer auction = new FakeAuctionServer("item-54321");
 		final String EXPECTED = "JOIN";
@@ -48,19 +65,18 @@ public class FakeAuctionServerTests {
 		assertEquals("Command with value ->JOIN<- should return a String \"JOIN\"", EXPECTED, ACTUAL);
 	}
 	
+  // <mlr 131031: this test passes in ant and in mvn>
 	@Test
-	public void getSniperCommandFromMessage_withCmdBID_returnsBID() {
+	public void getSniperCommandFromMessage_withCmdBID_returnsBIDString() {
 		
 		FakeAuctionServer auction = new FakeAuctionServer("item-54321");
 		final String EXPECTED = "BID";
 		final String ACTUAL = auction.getSniperCommandFromMessage("SQLVersion: 1.1; Command: BID;");
 		assertEquals("Command with value ->BID<- should return a String \"BID\"", EXPECTED, ACTUAL);
 	}
-
-	@Test
-	@Parameters({"AAA,1", "BBB,2"})
-	public void params_in_annotation(String p1, Integer p2) { }
-
+  */
+  /************************************* END - use junitparams to parameterize these 2 tests */
+  
 	/*
 	@Parameters({"SQLVersion: 1.1; Command: JOIN;,JOIN",
 		           "SQLVersion: 1.1; Command: BID;,BID"})
@@ -70,21 +86,14 @@ public class FakeAuctionServerTests {
 	public void getSniperCommandFromMessage_calledWithACmd_returnsThatCmd(String message, String expectedResult) {
 	public void getSniperCommandFromMessage_calledWithACmd_returnsThatCmd(String message) {
 	*/
+	
+	/*
+	// <mlr 131031: this test NPEs in ant BUT PASSES in mvn> <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 	@Test
 	@Parameters({"SQLVersion: 1.1; Command: JOIN;,JOIN",
 		           "SQLVersion: 1.1; Command: BID;,BID"})
-
-	public void getSniperCommandFromMessage_calledWithACmd_returnsThatCmd(String message, String expectedResult) {
-		//FakeAuctionServer auction = new FakeAuctionServer("item-54321");
-		//assertNotNull(expectedResult);
-		assertNotNull(message);
-		
-		String messageHW = (message == null) ? "Hello, world!" : message; // ??? :-(
-		//String messageHW = "Hello, world!";
-		
-		assertEquals("-->" + messageHW.toString() + "<--", "AAA", messageHW);
-		
-		/*
+	public void getSniperCommandFromMessage_calledWithACmd_returnsCmdString(String message, String expectedResult) {
+		FakeAuctionServer auction = new FakeAuctionServer("item-54321");
 		String EXPECTED = expectedResult;
 		String ACTUAL = auction.getSniperCommandFromMessage(message);
 		assertEquals("Command with value ->"
@@ -93,6 +102,6 @@ public class FakeAuctionServerTests {
 		             + EXPECTED 
 		             + "\"", 
 		             EXPECTED, ACTUAL);
-		             */
 	}
+	*/
 }
