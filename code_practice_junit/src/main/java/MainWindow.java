4b9dc2895d58e92b42f28a7ab23efc59ee6a800b
Mike Rocha <michaelr@vifla.com>
2013-11-14 13:58:05 +0000

    michaelr - GOOS, p. 96a; I took a crack at solving the acceptance test failures due to Window Licker. The 1st failure came from lack of a MainWindow JFrame. I believe I fixed that.
    So, I'm checking in this version done on my own. Next, I'll compare it with the book's version.

--- code_practice_junit/src/main/java/MainWindow.java
+++ code_practice_junit/src/main/java/MainWindow.java
@@ -1,4 +1,23 @@
-public class MainWindow {
+import java.awt.Container;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+public class MainWindow extends JFrame {
 	
-	public static final String MAIN_WINDOW_NAME = "MainWindow";
+	public static final String MAIN_WINDOW_NAME = "Auction Sniper Main";
+	
+	MainWindow() {
+		super(MAIN_WINDOW_NAME);
+	}
+	
+ 	void setGui() {
+ 		try {
+ 			setLocation(0, 100);
+ 			setMinimumSize(new Dimension(350, 150));
+ 			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ 			Container cp = getContentPane();
+ 		} catch (Exception e) {
+ 			e.printStackTrace();
+ 		}
+ 	}
 }
