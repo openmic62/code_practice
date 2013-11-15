331b0d8448c44dc0f386038e1516626fe7668e07
Mike Rocha <michaelr@vifla.com>
2013-11-15 21:20:17 +0000

    michaelr - GOOS, p. 98b; this version contains my version of Sniper Main that connects and send a JOIN message to the FakeAuctionServer.

--- code_practice_junit/src/main/java/Main.java
+++ code_practice_junit/src/main/java/Main.java
@@ -1,5 +1,14 @@
 import javax.swing.SwingUtilities;
 
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.ChatManager;
+import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.ConnectionConfiguration.SecurityMode;
+import org.jivesoftware.smack.MessageListener;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.packet.Message;
+
 public class Main {
 	private MainWindow ui;
 
@@ -7,14 +16,27 @@ public class Main {
 	public static final String STATUS_JOINING = "Joining auction";
 	public static final String STATUS_LOST = "Lost auction";
 	
-	public Main() {
+	private int port = 5222;
+	private String hostname;
+	private ChatManager chatManager;
+	private ConnectionConfiguration config;
+	private XMPPConnection connection;
+
+	public Main() throws XMPPException, Exception {
+		hostname = java.net.InetAddress.getLocalHost().getHostName();
 		startUserInterface();
+		config = createConnectionConfiguration(hostname, port);
+		connection = createConnection(config);
+		connection.connect();
+		performLogin("sniper", "sniper");
+		chatManager = connection.getChatManager();
+		sendMessage("JOIN", "auction-item-54321@" + hostname, new SniperMessageListener());
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws XMPPException, Exception {
 		Main main = new Main();
 	}
-	
+
 	private void startUserInterface() {
 		try {
 			SwingUtilities.invokeAndWait(new Runnable() {
@@ -24,6 +46,34 @@ public class Main {
 			});
 		} catch (Exception e) {
 			e.printStackTrace();
+		}
+	}
+	
+	private ConnectionConfiguration createConnectionConfiguration(String server, int port) {
+		config = new ConnectionConfiguration(server, port);
+		config.setSASLAuthenticationEnabled(false);
+		config.setSecurityMode(SecurityMode.disabled);
+		return config;
+	}
+	
+	private XMPPConnection createConnection(ConnectionConfiguration config) throws XMPPException {
+		return new XMPPConnection(config);
+	}
+	
+	private void performLogin(String username, String password) throws XMPPException {
+		if (connection!=null && connection.isConnected()) {
+			connection.login(username, password);
+		}
+	}
+
+	private void sendMessage(String message, String buddyJID, MessageListener messageListener) throws XMPPException {
+		Chat chat = chatManager.createChat(buddyJID, messageListener);
+		chat.sendMessage(message);
+	}
+	
+	class SniperMessageListener implements MessageListener {
+		@Override
+		public void processMessage(Chat chat, Message message) {
 		}
 	}
 }
