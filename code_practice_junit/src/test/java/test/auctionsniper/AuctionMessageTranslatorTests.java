91455a00cfa8e84be3c95b38d1229d4cb9f1e523
Mike <Mike@50d94a75-6656-bf47-8487-448895b3180b>
2013-11-29 15:57:23 +0000

    michaelr - moved the new AuctionMessageTranslator stuff into the Java package structure.
    git-svn-id: svn://localhost/code_practice/trunk@121 50d94a75-6656-bf47-8487-448895b3180b

--- code_practice_junit/src/test/java/test/auctionsniper/AuctionMessageTranslatorTests.java
+++ code_practice_junit/src/test/java/test/auctionsniper/AuctionMessageTranslatorTests.java
@@ -1,8 +1,13 @@
 package test.auctionsniper;
 
+import org.jivesoftware.smack.Chat;
+
 import org.junit.Test;
 
 public class AuctionMessageTranslatorTests {
+	public static final Chat UNUSED_CHAT = null;
+	private final AuctionMessageTranslator translator = new AuctionMessageTranslator();
+	
 	@Test
 	public void notifiesAuctionClosedWhenCloseMessageReceived() {
 	}
