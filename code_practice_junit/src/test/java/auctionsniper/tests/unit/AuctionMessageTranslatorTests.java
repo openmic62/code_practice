78005eea7d25d0f41afa3489cb86cf3cd2ab8c0f
Mike Rocha <openmic62@gmail.com>
2013-12-01 20:37:12 +0000

    michaelr - GOOS, p. 116b; implemented using the latest JMock API.
    -----------------------------------------------------------------------------------
    ///@RunWith(JMock.class)
    public class AuctionMessageTranslatorTests {
    	public static final Chat UNUSED_CHAT = null;
    	private final AuctionMessageTranslator translator = new AuctionMessageTranslator();
    	
    	///private final Mockery context = new Mockery();
    	///private final Mockery context = new JUnit4Mockery();
    	@Rule public final JUnitRuleMockery context = new JUnitRuleMockery();
    -----------------------------------------------------------------------------------
    
    This fixes the deprecated API warning I was getting.
    G:\student\code_practice_junit>javac -Xlint:deprecation -cp %CLASSPATH%;%SC%;%TC% -d %TC% %TD%\auctionsniper\tests\\unit\AuctionMessageTranslatorTests.java
    src\test\java\auctionsniper\tests\unit\AuctionMessageTranslatorTests.java:91: warning: [deprecation] JMock in org.jmock.integration.junit4 has been deprecated
    import org.jmock.integration.junit4.JMock;
                                       ^
    src\test\java\auctionsniper\tests\unit\AuctionMessageTranslatorTests.java:96: warning: [deprecation] JMock in org.jmock.integration.junit4 has been deprecated
    @RunWith(JMock.class)
             ^
    2 warnings

--- code_practice_junit/src/test/java/auctionsniper/tests/unit/AuctionMessageTranslatorTests.java
+++ code_practice_junit/src/test/java/auctionsniper/tests/unit/AuctionMessageTranslatorTests.java
@@ -88,18 +88,23 @@ import org.jivesoftware.smack.Chat;
 import org.jivesoftware.smack.packet.Message;
 
 import org.jmock.Expectations;
-import org.jmock.integration.junit4.JMock;
+///import org.jmock.integration.junit4.JMock;
+///import org.jmock.integration.junit4.JUnit4Mockery;
+import org.jmock.integration.junit4.JUnitRuleMockery;
 import org.jmock.Mockery;
+
+import org.junit.Rule;
 import org.junit.runner.RunWith;
 import org.junit.Test;
 
-@RunWith(JMock.class)
+///@RunWith(JMock.class)
 public class AuctionMessageTranslatorTests {
 	public static final Chat UNUSED_CHAT = null;
 	private final AuctionMessageTranslator translator = new AuctionMessageTranslator();
 	
+	///private final Mockery context = new Mockery();
 	///private final Mockery context = new JUnit4Mockery();
-	private final Mockery context = new Mockery();
+	@Rule public final JUnitRuleMockery context = new JUnitRuleMockery();
 	private final AuctionEventListener listener = context.mock(AuctionEventListener.class);
 	
 	@Test public void 
