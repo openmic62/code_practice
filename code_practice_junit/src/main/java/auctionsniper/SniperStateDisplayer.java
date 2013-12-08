5862c9925e1fb4e3e63b24cef97a9ab1ba5f021b
Mike <Mike@50d94a75-6656-bf47-8487-448895b3180b>
2013-12-08 06:03:03 +0000

    michaelr - GOOS, p. 134a; made my code look like the book's.
    git-svn-id: svn://localhost/code_practice/trunk@143 50d94a75-6656-bf47-8487-448895b3180b

---
+++ code_practice_junit/src/main/java/auctionsniper/SniperStateDisplayer.java
@@ -0,0 +1,37 @@
+package auctionsniper;
+
+import javax.swing.SwingUtilities;
+
+public class SniperStateDisplayer implements SniperListener {
+
+	private MainWindow ui;
+	
+	SniperStateDisplayer(MainWindow mainWindow) {
+		this.ui = mainWindow;
+	}
+	
+	// Implement the SniperListener interface
+	@Override
+	public void sniperBidding() {
+  	showStatus(Main.STATUS_BIDDING);
+  }
+ 	
+	@Override
+	public void sniperLost() {
+  	showStatus(Main.STATUS_LOST);
+ 	}
+ 	
+	@Override
+	public void sniperWinning() {
+  	showStatus(Main.STATUS_WINNING);
+ 	}
+ 	
+ 	private void showStatus(final String status) {
+  	SwingUtilities.invokeLater(new Runnable() {
+  		@Override
+  		public void run() {
+  			ui.showStatus(status);
+  		}
+  	});
+ 	} 		
+}	
