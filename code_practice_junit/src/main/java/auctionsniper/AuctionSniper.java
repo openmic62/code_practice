7cc63ac4a5b4d88c197ef3d0f384fdd4d626aabe
michaelr <michaelr@50d94a75-6656-bf47-8487-448895b3180b>
2013-12-20 12:57:00 +0000

    michaelr - GOOS, p. 147b; this version contains my implementation of "The Sniper Wins".
    - I cheated a bit by glancing at the book's implementation a couple of days ago.
    - However, I spent time thinking through the design from end-to-end. That gave me a big boost of confidence.
    - Also, with my first implementation, the existing test "reportLostIfAuctionClosesImmediately" broke. In other words, I caught a regression. THAT IS FUCKING AWESOME!!!!!!.
    
    >>>>>>>>>>>>>>> I AM INVINCIBLE !!! <<<<<<<<<<<<<<<<<
    
    git-svn-id: svn://localhost/code_practice/trunk@158 50d94a75-6656-bf47-8487-448895b3180b

--- code_practice_junit/src/main/java/auctionsniper/AuctionSniper.java
+++ code_practice_junit/src/main/java/auctionsniper/AuctionSniper.java
@@ -4,6 +4,7 @@ public class AuctionSniper implements AuctionEventListener {
 	
 	private Auction auction;
 	private SniperListener sniperListener;
+	private String sniperState = "JOINING";
 	
 	public AuctionSniper(Auction a, SniperListener sl) {
 		this.auction = a;
@@ -16,15 +17,24 @@ public class AuctionSniper implements AuctionEventListener {
 	}*/
 	
 	public void auctionClosed(){
-		sniperListener.sniperLost();
+		if ("WINNING".equals(sniperState)) {
+		 	sniperListener.sniperWon();
+		} else if ("BIDDING".equals(sniperState)) {
+		 	sniperListener.sniperLost();
+		} else if ("JOINING".equals(sniperState)) {
+		 	sniperListener.sniperLost();
+		}
+		///sniperListener.sniperLost();
 	}
 	
 	public void currentPrice(int price, int increment, PriceSource source){
 		switch (source) {
 			case FromSniper: 
+				sniperState = "WINNING";
 		 	  sniperListener.sniperWinning();
 				break;
 			case FromOtherBidder:
+				sniperState = "BIDDING";
 				auction.bid(price + increment);
 		 	  sniperListener.sniperBidding();
 				break;
