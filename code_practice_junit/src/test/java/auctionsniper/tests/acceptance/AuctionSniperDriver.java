1d767f1eb49f2605cd4e8dad47601c61aa9a6629
Mike Rocha <openmic62@gmail.com>
2014-01-13 03:19:54 +0000

    michaelr - GOOS, p. 184b; I studied JTextField and JButton in the Oracle tutorials. Then, I thought about how we were going to do this. I developed a pretty good idea, but (as usual) there were several viable ways to proceed. So, I decided to follow the BV without doing MV.
    I need to speed things along ... Canon employment seems very uncertain to me right now.

--- code_practice_junit/src/test/java/auctionsniper/tests/acceptance/AuctionSniperDriver.java
+++ code_practice_junit/src/test/java/auctionsniper/tests/acceptance/AuctionSniperDriver.java
@@ -5,14 +5,18 @@ import auctionsniper.MainWindow;
 
 import com.objogate.wl.gesture.Gestures;
 import com.objogate.wl.swing.AWTEventQueueProber;
+import com.objogate.wl.swing.driver.JButtonDriver;
 import com.objogate.wl.swing.driver.JFrameDriver;
 import com.objogate.wl.swing.driver.JLabelDriver;
 import com.objogate.wl.swing.driver.JTableDriver;
 import com.objogate.wl.swing.driver.JTableHeaderDriver;
+import com.objogate.wl.swing.driver.JTextFieldDriver;
 import com.objogate.wl.swing.gesture.GesturePerformer;
 import com.objogate.wl.swing.matcher.IterableComponentsMatcher;
 
+import javax.swing.JButton;
 import javax.swing.table.JTableHeader;
+import javax.swing.JTextField;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -24,7 +28,7 @@ public class AuctionSniperDriver extends JFrameDriver {
 	static Logger logger = LogManager.getLogger(AuctionSniperDriver.class.getName());	
 	
 	public AuctionSniperDriver() {
-		this(1000);
+		this(1500);
 	}
 	
 	@SuppressWarnings("unchecked")
@@ -35,6 +39,26 @@ public class AuctionSniperDriver extends JFrameDriver {
 		      	showingOnScreen()),
 		      new AWTEventQueueProber(timeoutMillis, 100));
 	}
+	
+	// <mlr 140112: begin - add items thru UI>
+	public void startBiddingFor(String itemId) {
+		itemIdField().replaceAllText(itemId);
+		bidButton().click();
+	}	
+	
+	@SuppressWarnings("unchecked")
+	private JTextFieldDriver itemIdField() {
+		JTextFieldDriver newItemId = 
+		  new JTextFieldDriver(this, JTextField.class, named(MainWindow.NEW_ITEM_ID_NAME));
+		newItemId.focusWithMouse();
+		return newItemId;
+	}
+	
+	@SuppressWarnings("unchecked")
+	private JButtonDriver bidButton() {
+		return new JButtonDriver(this, JButton.class, named(MainWindow.JOIN_BUTTON_NAME));
+	}
+	// <mlr 140112: end - add items thru UI>
 	
 	@SuppressWarnings("unchecked")
 	public void showSniperStatus(String statusText) {
