8e4da51ebb2c3575bc86b20de3bc9f507b7ebe10
Mike Rocha <openmic62@gmail.com>
2014-01-14 00:24:32 +0000

    michaelr - GOOS, p. 186b;
    - acquired MainWindowTests.java
    - changed build.xml so that it can run integration tests
    - changed build.xml so that I can now run any combination of tests:
      - unit
      - integration
      - acceptance
      - unit, integration
      - unit, acceptance
      - integration, acceptance
      - unit, integration, acceptance

---
+++ code_practice_junit/src/test/java/auctionsniper/tests/integration/MainWindowTests.java
@@ -0,0 +1,106 @@
+/**
+ *
+ * <u>Growing Object-Oriented Software, Guided by Tests</u><br>
+ *    Part III: A Worked Example<br>
+ * 		Ch. 16 Sniping for Multiple Items<br>
+ *    Another Level of Testing<br>
+ *    p. 186b
+ *
+ * MainWindow holds the responsibility for parsing messages from the
+ * Southabee's auction into a Sniper event on which this application can act.
+
+ -------------------------------------------------------------------------------
+
+ --------------- which java ------------------
+ for %i in (java.exe) do @echo.   %~$PATH:i
+ --------------- which java ------------------
+ 
+ For JMockit java.exe MUST be of JDK (with attach.dll) like
+   C:\Program Files\Java\jdk1.7.0_02\bin\java.exe
+ NOT the JRE one (missing attach.dll) Java automatically installs at
+   C:\Windows\system32\java.exe
+   
+ Work the System PATH variable to ensure correct java.exe gets used.
+ PATH = (system) PATH + (user) PATH - in that specific order
+ 
+ -------------------------------------------------------------------------------
+ 
+ C:\Users\michaelr>h:
+ ***** Abstract the ENV
+ H:\> copy and paste the following lines straight onto the command line
+      NOTE: 131117 - SIH is short for SYSINTERNALS_HOME 
+                     (usually =C:\Users\Mike\Downloads\SysinternalsSuite in system ENV)
+ 
+ set CLASSPATH=lib;lib\Smack.jar;lib\Smackx.jar;lib\Smackx-debug.jar;lib\junit-4.11.jar;lib\hamcrest-all-1.3.jar
+ set WL=lib\windowlicker-core-DEV.jar;lib\windowlicker-swing-DEV.jar
+ set CLASSPATH=%WL%;%CLASSPATH%
+ set JM=lib\jmock-2.6.0.jar;lib\jmock-junit4-2.6.0.jar
+ set CLASSPATH=%JM%;%CLASSPATH%
+ set L4J2=lib\log4j-api-2.0-beta9.jar;lib\log4j-core-2.0-beta9.jar
+ set CLASSPATH=%L4J2%;%CLASSPATH%
+ set ACL3=lib\commons-lang3-3.1.jar
+ set CLASSPATH=%ACL3%;%CLASSPATH%
+ set SIH=src\test\scripts\SysinternalsSuite_131101
+ set SC=target\classes
+ set TC=target\test-classes
+ set SD=src\main\java
+ set TD=src\test\java
+ g:
+ cd student\code_practice_junit
+ javac -cp %CLASSPATH%;%SC% -d %SC% %SD%\auctionsniper\MainWindow.java
+ javac -cp %CLASSPATH%;%SC%;%TC% -d %TC% %TD%\auctionsniper\tests\integration\MainWindowTests.java
+ java  -cp %CLASSPATH%;%SC%;%TC% org.junit.runner.JUnitCore auctionsniper.tests.integration.MainWindowTests
+ ant runtest -DtestClass=MainWindowTests
+ 
+ ***** build the MainWindow source file
+ H:\>cd student\code_practice_junit
+ H:\student\code_practice_junit>l
+ H:\student\code_practice_junit>echo %CLASSPATH%
+ H:\student\code_practice_junit>set MW_FILES=MainWindow.java
+ H:\student\code_practice_junit>echo %MW_FILES%
+ H:\student\code_practice_junit>javac -cp %CLASSPATH%;%SC% -d %SC% %SD%\auctionsniper\%MW_FILES%
+                                javac -cp %CLASSPATH%;%SC% -d %SC% %SD%\auctionsniper\MainWindow.java
+                                
+ ***** build the Tests MainWindowTests
+ H:\student\code_practice_junit>javac -cp %CLASSPATH%;%SC%;%TC% -d %TC% %TD%\auctionsniper\tests\integration\MainWindowTests.java
+ 
+ ***** run the Tests (command line Java)
+ H:\student\code_practice_junit>java  -cp %CLASSPATH%;%SC%;%TC% org.junit.runner.JUnitCore auctionsniper.tests.integration.MainWindowTests
+ 
+ ***** run the Tests (command line Ant)
+ H:\student\code_practice_junit>ant clean_all
+ H:\student\code_practice_junit>ant runtest -DtestClass=MainWindowTests
+
+ ***** run the Tests (command line Maven)
+ H:\student\code_practice_junit>mvn antrun:run test -Dtest=MainWindowTests
+ */ 
+package auctionsniper.tests.integration;
+
+import auctionsniper.MainWindow;
+import auctionsniper.SnipersTableModel;
+
+import auctionsniper.tests.AuctionSniperTestUtilities;
+import auctionsniper.tests.acceptance.AuctionSniperDriver;
+
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.packet.Message;
+
+import org.jmock.Expectations;
+import org.jmock.integration.junit4.JUnitRuleMockery;
+
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class MainWindowTests {
+	
+	@Rule public final JUnitRuleMockery context = new JUnitRuleMockery();
+	
+	private final SnipersTableModel tableModel = new SnipersTableModel();
+	private final MainWindow mainWindow = new MainWindow(tableModel);
+	private final AuctionSniperDriver driver = new AuctionSniperDriver(100);
+	
+	@Test public void 
+	makesUserRequestWhenJoinButtonClicked() {
+	}
+}
