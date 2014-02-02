e87b6c3f4b0fb1fae2329ba58b30837c324de58b
Mike <Mike@50d94a75-6656-bf47-8487-448895b3180b>
2014-02-02 17:53:56 +0000

    michaelr - GOOS, p. 191b; returned here from p. 195b so that I could refactor MainWindow and SnipersTableModel in to package auctionsniper.ui.
    This commit is stable with XMPPAuctionTests.java created, but not finished yet because on p. 195b the book puts XMPPAuction into a separate package auctionsniper.xmpp.
    
    After this commit, I will do the first package refactoring with auctionsniper.ui.
    
    git-svn-id: svn://localhost/code_practice/trunk@205 50d94a75-6656-bf47-8487-448895b3180b

---
+++ code_practice_junit/src/test/java/auctionsniper/tests/integration/XMPPAuctionTests.java
@@ -0,0 +1,112 @@
+/**
+ *
+ * <u>Growing Object-Oriented Software, Guided by Tests</u><br>
+ *    Part III: A Worked Example<br>
+ * 		Ch. 17 Teasing Apart Main<br>
+ *    Writing a New Test (for XMPPAuction - expanded)<br>
+ *    p. 195a
+ *
+ * XMPPAcution now holds the responsibility for all communications with
+ * Southabee's auction using an XMPP Chat.
+
+ -------------------------------------------------------------------------------
+
+ --------------- which java ------------------
+ for %i in (java.exe) do @echo.   %~$PATH:i
+ --------------- which java ------------------
+ 
+ For JMockit java.exe MUST be of JDK (with attach.dll) like
+   C:\Program Files\Java\jdk1.7.0_02\bin\java.exe
+ NOT the JRE one (missing attach.dll) Java automatically installs at
+   C:\Windows\system32\java.exe
+   
+ Work the System PATH variable to ensure correct java.exe gets used.
+ PATH = (system) PATH + (user) PATH - in that specific order
+ 
+ -------------------------------------------------------------------------------
+ 
+ C:\Users\michaelr>h:
+ ***** Abstract the ENV
+ H:\> copy and paste the following lines straight onto the command line
+      NOTE: 131117 - SIH is short for SYSINTERNALS_HOME 
+                     (usually =C:\Users\Mike\Downloads\SysinternalsSuite in system ENV)
+ e:
+ set CLASSPATH=lib;lib\Smack.jar;lib\Smackx.jar;lib\Smackx-debug.jar;lib\junit-4.11.jar;lib\hamcrest-all-1.3.jar
+ set WL=lib\windowlicker-core-DEV.jar;lib\windowlicker-swing-DEV.jar
+ set CLASSPATH=%WL%;%CLASSPATH%
+ set JM=lib\jmock-2.6.0.jar;lib\jmock-junit4-2.6.0.jar
+ set CLASSPATH=%JM%;%CLASSPATH%
+ set L4J2=lib\log4j-api-2.0-beta9.jar;lib\log4j-core-2.0-beta9.jar
+ set CLASSPATH=%L4J2%;%CLASSPATH%
+ set ACL3=lib\commons-lang3-3.1.jar
+ set CLASSPATH=%ACL3%;%CLASSPATH%
+ set SIH=src\test\scripts\SysinternalsSuite_131101
+ set SC=target\classes
+ set TC=target\test-classes
+ set SD=src\main\java
+ set TD=src\test\java
+
+ cd student\code_practice_junit
+ javac -cp %CLASSPATH%;%SC% -d %SC% %SD%\auctionsniper\XMPPAuction.java
+ javac -cp %CLASSPATH%;%SC%;%TC% -d %TC% %TD%\auctionsniper\tests\integration\XMPPAuctionTests.java
+ java  -cp %CLASSPATH%;%SC%;%TC% org.junit.runner.JUnitCore auctionsniper.tests.integration.XMPPAuctionTests
+ ant runtest -DtestClass=XMPPAuctionTests
+ 
+ ***** build the XMPPAuction source file
+ H:\>cd student\code_practice_junit
+ H:\student\code_practice_junit>l
+ H:\student\code_practice_junit>echo %CLASSPATH%
+ H:\student\code_practice_junit>set XA_FILES=XMPPAuction.java
+ H:\student\code_practice_junit>echo %XA_FILES%
+ H:\student\code_practice_junit>javac -cp %CLASSPATH%;%SC% -d %SC% %SD%\auctionsniper\%XA_FILES%
+                                javac -cp %CLASSPATH%;%SC% -d %SC% %SD%\auctionsniper\XMPPAuction.java
+                                
+ ***** build the Tests XMPPAuctionTests
+ H:\student\code_practice_junit>javac -cp %CLASSPATH%;%SC%;%TC% -d %TC% %TD%\auctionsniper\tests\integration\XMPPAuctionTests.java
+ 
+ ***** run the Tests (command line Java)
+ H:\student\code_practice_junit>java  -cp %CLASSPATH%;%SC%;%TC% org.junit.runner.JUnitCore auctionsniper.tests.integration.XMPPAuctionTests
+ 
+ ***** run the Tests (command line Ant)
+ H:\student\code_practice_junit>ant clean_all
+ H:\student\code_practice_junit>ant runtest -DtestClass=XMPPAuctionTests
+
+ ***** run the Tests (command line Maven)
+ H:\student\code_practice_junit>mvn antrun:run test -Dtest=XMPPAuctionTests
+ */ 
+package auctionsniper.tests.integration;
+
+import auctionsniper.Auction;
+import auctionsniper.XMPPAuction;
+import auctionsniper.AuctionEventListener;
+
+import auctionsniper.tests.AuctionSniperTestUtilities;
+import auctionsniper.tests.acceptance.FakeAuctionServer;
+
+import com.objogate.wl.swing.probe.ValueMatcherProbe;
+
+import java.util.concurrent.CountDownLatch;
+
+import org.jmock.Expectations;
+import org.jmock.integration.junit4.JUnitRuleMockery;
+
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.equalTo;
+
+public class XMPPAuctionTests {
+	
+	private  FakeAuctionServer auctionServer    = new FakeAuctionServer(AuctionSniperTestUtilities.ITEM_ID1);
+
+	@Rule public final JUnitRuleMockery context = new JUnitRuleMockery();
+	private final AuctionEventListener listener = context.mock(AuctionEventListener.class);
+
+	@Test public void 
+	receivesEventsFromAuctionServerAfterJoining() throws Exception{
+		CountDownLatch auctionWasClosed = new CountDownLatch(1);
+		
+		//Auction auction = new XMPPAuction(connection, auctionServer.getItemID());
+	}
+}
