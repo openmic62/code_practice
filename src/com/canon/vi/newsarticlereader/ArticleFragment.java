ccad25d20826b7d235eba326c96de56c885006b9
michaelr <michaelr@vi-miker6430s.VIRTUALIMAGING.OFFICE>
2015-01-08 12:20:19 -0500

    removed one last unused import that I missed.

--- src/com/canon/vi/newsarticlereader/ArticleFragment.java
+++ src/com/canon/vi/newsarticlereader/ArticleFragment.java
@@ -1,16 +1,15 @@
 package com.canon.vi.newsarticlereader;
 
-import com.canon.vi.newsarticlereader.content.ArticlesContent;
-
 import android.app.Activity;
+import android.app.Fragment;
 import android.net.Uri;
 import android.os.Bundle;
-import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
+
+import com.canon.vi.newsarticlereader.content.ArticlesContent;
 
 /**
  * A simple {@link Fragment} subclass. Activities that contain this fragment
