90b975185edadd769c7d3bf4668a5cd3ba0fda2d
Mike Rocha <openmic62@gmail.com>
2015-02-14 20:10:05 -0500

    NAP - p.26a: 01_dev, wired up QuizActivity button click handlers.

--- nerd_ap/01m_GeoQuiz/gen/com/bignerdranch/android/geoquiz/R.java
+++ nerd_ap/01m_GeoQuiz/gen/com/bignerdranch/android/geoquiz/R.java
@@ -1635,6 +1635,7 @@ containing a value of this type.
         public static final int ic_launcher=0x7f020033;
     }
     public static final class id {
+        public static final int LinearLayout1=0x7f09003f;
         public static final int action_bar=0x7f090031;
         public static final int action_bar_activity_content=0x7f090003;
         public static final int action_bar_container=0x7f090030;
@@ -1648,7 +1649,7 @@ containing a value of this type.
         public static final int action_mode_bar=0x7f09002e;
         public static final int action_mode_bar_stub=0x7f09002d;
         public static final int action_mode_close_button=0x7f090020;
-        public static final int action_settings=0x7f09003f;
+        public static final int action_settings=0x7f090042;
         public static final int activity_chooser_view_content=0x7f090021;
         public static final int always=0x7f090014;
         public static final int beginning=0x7f09001b;
@@ -1663,6 +1664,7 @@ containing a value of this type.
         public static final int end=0x7f09001c;
         public static final int expand_activities_button=0x7f090022;
         public static final int expanded_menu=0x7f090028;
+        public static final int false_button=0x7f090041;
         public static final int home=0x7f090000;
         public static final int homeAsUp=0x7f09000e;
         public static final int icon=0x7f090026;
@@ -1695,6 +1697,7 @@ containing a value of this type.
         public static final int submit_area=0x7f09003c;
         public static final int tabMode=0x7f09000c;
         public static final int title=0x7f090027;
+        public static final int true_button=0x7f090040;
         public static final int up=0x7f090001;
         public static final int useLogo=0x7f090013;
         public static final int withText=0x7f090018;
@@ -1749,7 +1752,7 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030014;
         public static final int abc_search_view=0x7f030015;
         public static final int abc_simple_dropdown_hint=0x7f030016;
-        public static final int activity_main=0x7f030017;
+        public static final int activity_quiz=0x7f030017;
         public static final int support_simple_spinner_dropdown_item=0x7f030018;
     }
     public static final class menu {
@@ -1812,9 +1815,13 @@ containing a value of this type.
         /**  Content description for the Toolbar icon used to collapse an expanded action mode. [CHAR LIMIT=NONE] 
          */
         public static final int abc_toolbar_collapse_description=0x7f0a0004;
-        public static final int action_settings=0x7f0a0012;
+        public static final int action_settings=0x7f0a0016;
         public static final int app_name=0x7f0a0010;
-        public static final int hello_world=0x7f0a0011;
+        public static final int correct_toast=0x7f0a0014;
+        public static final int false_button=0x7f0a0013;
+        public static final int incorrect_toast=0x7f0a0015;
+        public static final int question_text=0x7f0a0011;
+        public static final int true_button=0x7f0a0012;
     }
     public static final class style {
         /** 
