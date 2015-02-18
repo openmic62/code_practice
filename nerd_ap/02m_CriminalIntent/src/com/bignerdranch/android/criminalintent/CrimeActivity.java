a84018eed7ccb7b55e97920ae5be69dbcea2f9a5
Mike Rocha <openmic62@gmail.com>
2015-02-17 21:34:38 -0500

    NAP - p.143b: fragment not working, needs debugging

--- nerd_ap/02m_CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ nerd_ap/02m_CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -1,6 +1,8 @@
 package com.bignerdranch.android.criminalintent;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBarActivity;
 
 public class CrimeActivity extends ActionBarActivity {
@@ -9,6 +11,15 @@ public class CrimeActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_crime);
+
+		FragmentManager fm = getSupportFragmentManager();
+		Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+
+		if (fragment == null) {
+			fragment = new CrimeFragment();
+			fm.beginTransaction().add(R.id.fragmentContainer, fragment)
+					.commit();
+		}
 	}
 
 }
