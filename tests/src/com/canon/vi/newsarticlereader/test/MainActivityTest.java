d387869e8db29be91a42e4851c455215518561da
Mike Rocha <michaelr@vifla.com>
2015-03-17 08:09:05 -0400

    michaelr - done reacquainting myself with Eclipse/Android/Junit

---
+++ tests/src/com/canon/vi/newsarticlereader/test/MainActivityTest.java
@@ -0,0 +1,11 @@
+package com.canon.vi.newsarticlereader.test;
+
+import com.canon.vi.newsarticlereader.MainActivity;
+
+import android.test.ActivityUnitTestCase;
+
+public class MainActivityTest extends ActivityUnitTestCase<MainActivity> {
+	public MainActivityTest() {
+		super(null);
+	}
+}
