3959315cbb2751d2d616d74d06f6623888dc8899
Mike Rocha <openmic62@gmail.com>
2015-03-20 21:52:11 -0400

    Ok, that was easy; just needed a few simple things (read about them b4)

--- sandbox/NewsArticleReader/tests/src/com/canon/vi/newsarticlereader/test/MainActivityTest.java
+++ sandbox/NewsArticleReader/tests/src/com/canon/vi/newsarticlereader/test/MainActivityTest.java
@@ -1,10 +1,11 @@
 package com.canon.vi.newsarticlereader.test;
 
-import org.junit.Test;
+//import org.junit.Test;
 
 import android.content.Intent;
 import android.test.ActivityUnitTestCase;
-import android.test.UiThreadTest;
+//import android.test.UiThreadTest;
+import android.test.suitebuilder.annotation.SmallTest;
 
 import com.canon.vi.newsarticlereader.MainActivity;
 
@@ -15,15 +16,43 @@ public class MainActivityTest extends ActivityUnitTestCase<MainActivity> {
 	private MainActivity mActivity;
 
 	public MainActivityTest() {
-		super(null);
+		///super(null);
+		super(MainActivity.class);
 	}
 
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
+		
+		Intent intent = new Intent(getInstrumentation().getTargetContext(),
+				MainActivity.class);
+		startActivity(intent, null, null);
+		//mActivity = getActivity();
+		mActivity = (MainActivity) getActivity();
+		
+	}
+	
+	@Override
+	protected void tearDown() throws Exception {
+		super.tearDown();
 	}
 
-	@Test
+	// @Test
+	// @UiThreadTest
+	// @Ignore
+	@SmallTest
+	public void testNothing() {
+		assertEquals(true, true);
+	}
+
+/**
+ * This is what I had, but it didn't work 150319. Then I Google'd
+ * "activityunittestcase example" and found
+ * Android Testing Example – Helloworld with ActivityUnitTestCase
+ * http://wptrafficanalyzer.in/blog/android-testing-example-helloworld-with-activityunittestcase/
+ * From that web page I got the ActivityUnitTestCaseApp and pActivityUnitTestCaseTests
+ * projects that succeeded running as "Android JUnit Test". So, I'm reverse engineering.
+ * 	@Test
 	// /@UiThreadTest
 	// @Ignore
 	public void testNothing() {
@@ -41,5 +70,5 @@ public class MainActivityTest extends ActivityUnitTestCase<MainActivity> {
 		mActivity = getActivity();
 		// assertEquals(true, true);
 	}
-
+*/
 }
