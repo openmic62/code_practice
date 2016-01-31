1a140534aeba0b47fa45d59d55e8396a76ea95ec
Michael L Rocha <mike.rocha@thalesesec.com>
2016-01-31 13:43:29 -0500

    Creating the Managed Bean https://netbeans.org/kb/docs/javaee/javaee-gettingstarted.html#Exercise_3a

---
+++ sandbox/SimpleEE6App/src/java/my/presentation/MessageView.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package my.presentation;
+
+import boundary.MessageFacade;
+import entities.Message;
+import javax.ejb.EJB;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author Mike
+ */
+@Named(value = "MessageView")
+@RequestScoped
+public class MessageView {
+
+    // Injects the MessageFacade session bean using the @EJB annotation
+    @EJB
+    private MessageFacade messageFacade;
+    
+    // Creates a new field
+    private Message message;
+
+    /**
+     * Creates a new instance of MessageView
+     */
+    public MessageView() {
+        this.message = new Message();
+    }
+    
+    // Calls getMessage to retrieve the message
+    public Message getMessage() {
+        return message;
+    }
+    
+    // Returns the total number of messages
+    public int getNumberOfMessages() {
+        return messageFacade.findAll().size();
+    }
+    
+    // Saves the message and the returns the string "theend"
+    public String postMessage() {
+        this.messageFacade.create(message);
+        return "theend";
+    }
+}
