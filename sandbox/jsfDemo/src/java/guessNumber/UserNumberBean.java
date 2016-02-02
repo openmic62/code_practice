90843babd9195b48eec28bd236a292565880afcc
Michael L Rocha <mike.rocha@thalesesec.com>
2016-02-01 20:22:57 -0500

    Adding properties [to the managed bean]. https://netbeans.org/kb/docs/web/jsf20-intro.html#addingProperties

--- sandbox/jsfDemo/src/java/guessNumber/UserNumberBean.java
+++ sandbox/jsfDemo/src/java/guessNumber/UserNumberBean.java
@@ -9,6 +9,8 @@ import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
 import java.util.Random;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -19,6 +21,8 @@ import java.util.Random;
 public class UserNumberBean implements Serializable {
 
     Integer randomInt;
+    Integer userNumber;
+    String response;
     
     /**
      * Creates a new instance of UserNumberBean
@@ -27,6 +31,30 @@ public class UserNumberBean implements Serializable {
         Random randomGR = new Random();
         randomInt = new Integer(randomGR.nextInt(10));
         System.out.println("Duke's number: " + randomInt);
+    }
+
+    public Integer getUserNumber() {
+        return userNumber;
+    }
+
+    public void setUserNumber(Integer userNumber) {
+        this.userNumber = userNumber;
+    }
+
+    public String getResponse() {
+        if ((userNumber != null) && (userNumber.compareTo(randomInt) == 0)) {
+            
+            // invalidate user session
+            FacesContext context = FacesContext.getCurrentInstance();
+            HttpSession session = (HttpSession) context.getExternalContext().getSession(false);
+            session.invalidate();
+            
+            return "Yay! You got it!";
+        } else {
+            
+            return "<p>Sorry, " + userNumber + " isn't it.</p>"
+                    + "<p>Guess again...</p>";
+        }
     }
     
 }
