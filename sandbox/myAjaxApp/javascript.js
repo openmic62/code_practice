17040ea4d6e8cf682b96056ddf7eaa6def72f426
Michael L Rocha <mike.rocha@thalesesec.com>
2016-02-06 20:00:07 -0500

    Updating the HTML DOM https://netbeans.org/kb/docs/web/ajax-quickstart.html#htmldom

--- sandbox/myAjaxApp/javascript.js
+++ sandbox/myAjaxApp/javascript.js
@@ -8,8 +8,15 @@
 var req;
 var isIE;
 
+var completeField;
+var completeTable;
+var autoRow;
+
 function init() {
     completeField = document.getElementById("complete-field");
+    completeTable = document.getElementById("complete-table");
+    autoRow = document.getElementById("auto-row");
+    completeTable.style.top = getElementY(autoRow) + "px";
 }
 
 function doCompletion() {
@@ -22,19 +29,99 @@ function doCompletion() {
 
 function initRequest() {
     if (window.XMLHttpRequest) {
-        if (navigator.userAgent.indexOf('MSIE') != -1) {
+        if (navigator.userAgent.indexOf('MSIE') !== -1) {
             isIE = true;
         }
         return new XMLHttpRequest();
     } else if (window.ActiveXObject) {
         isIE = true;
-        return new ActiveXObject("Microsoft.XMLHTTP")
+        return new ActiveXObject("Microsoft.XMLHTTP");
     }
 }
 
 function callback() {
-    if (req.readyState == 4)
-        if (req.status == 200) {
+    
+    clearTable();
+    
+    if (req.readyState === 4)
+        if (req.status === 200) {
             parseMessages(req.responseXML);
         }
+}
+
+function appendComposer(firstName, lastName, composerId) {
+    
+    var row;
+    var cell;
+    var linkElement;
+    
+    if (isIE) {
+        completeTable.style.display = 'block';
+        row = completeTable.insertRow(completeTable.rows.length);
+        cell = row.insertCell(0);
+    } else {
+        completeTable.style.display = 'table';
+        row = document.createElement("tr");
+        cell = document.createElement("td");
+        row.appendChild(cell);
+        completeTable.appendChild(row);
+    }
+    
+    cell.className = "popupCell";
+    
+    linkElement = document.createElement("a");
+    linkElement.className = "popupItem";
+    linkElement.setAttribute("href", "autocomplete?action=lookup&id=" + composerId);
+    linkElement.appendChild(document.createTextNode(firstName + " " + lastName));
+    cell.appendChild(linkElement);
+}
+
+function getElementY(element) {
+    
+    var targetTop = 0;
+    
+    if (element.offsetParent) {
+        while (element.offsetParent) {
+            targetTop += element.offsetTop;
+            element = element.offsetParent;
+        }
+    } else if (element.y) {
+        targetTop += element.y;
+    }
+    return targetTop;
+}
+
+function clearTable() {
+    if (completeTable.getElementsByTagName("tr").length > 0) {
+        completeTable.style.display = 'none';
+        for (loop = completeTable.childNodes.length -1; loop >= 0 ; loop--) {
+            completeTable.removeChild(completeField.childNodes[loop]);
+        }
+    }
+}
+
+function parseMessages(responseXML) {
+    
+    // no matches returned
+    if (responseXML === null) {
+        return false;
+    } else {
+        
+        var composers = responseXML.getElementsByTagName("composers")[0];
+        
+        if (composers.childNodes.length > 0) {
+            completeTable.setAttribute("bordercolor", "black");
+            completeTable.setAttribute("border", "1");
+            
+            for (loop = 0; loop < composers.childNodes.length; loop++) {
+                var composer = composer.childNodes[loop];
+                var firstName = composer.getElementsByTagName("firstName")[0];
+                var lastName = composer.getElementsByTagName("lastName")[0];
+                var composerId = composer.getElementsByTagName("composerId")[0];
+                appendComposer(firstName.childNodes[0].nodeValue,
+                    lastName.childNodes[0].nodeValue,
+                    composerId.childNodes[0].nodeValue);
+            }
+        }
+    }
 }
