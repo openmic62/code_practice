0c3f629b25fd68a51ef191f075654482b06950da
mikerocha <mikerocha@mikes-mbp.hsd1.fl.comcast.net>
2016-10-03 20:47:36 -0400

    Using Hibernate in a Web Application Creating the FilmHelper.java Helper Class Creating the Class ... FilmHelper.java https://netbeans.org/kb/docs/web/hibernate-webapp.html#06

---
+++ sandbox/DVDStore/src/java/dvdrental/FilmHelper.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dvdrental;
+
+import org.hibernate.Session;
+
+/**
+ *
+ * @author mikerocha
+ */
+public class FilmHelper {
+
+    Session session = null;
+
+    public FilmHelper() {
+        this.session = HibernateUtil.getSessionFactory().getCurrentSession();
+    }
+}
