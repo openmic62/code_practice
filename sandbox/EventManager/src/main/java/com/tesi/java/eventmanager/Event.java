99848a9db639eb56b5498946002b35a3fc4051e8
mikerocha <mikerocha@mikes-mbp.hsd1.fl.comcast.net>
2016-10-15 20:46:21 -0400

    1.1.2 The First Class http://docs.jboss.org/hibernate/orm/4.3/manual/en-US/html_single/#tutorial-firstapp-firstclass

---
+++ sandbox/EventManager/src/main/java/com/tesi/java/eventmanager/Event.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tesi.java.eventmanager;
+
+import java.util.Date;
+
+/**
+ *
+ * @author mikerocha
+ */
+public class Event {
+    private long id;
+    
+    private String title;
+    private Date date;
+
+    public Event() {}
+    
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }   
+}
