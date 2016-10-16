c204b2afb3ad877ef03d10c54db6579a71fa0252
mikerocha <mikerocha@mikes-mbp.hsd1.fl.comcast.net>
2016-10-16 12:13:01 -0400

    1.1.6. Startup and helpers ... HibernateUtil.java edited to reflect BV ... added log4j.properties copied from     https://www.mkyong.com/hibernate/how-to-configure-log4j-in-hibernate-project/ http://docs.jboss.org/hibernate/orm/4.3/manual/en-US/html_single/#tutorial-firstapp-helpers

--- sandbox/EventManager/src/main/java/com/tesi/java/eventmanager/util/HibernateUtil.java
+++ sandbox/EventManager/src/main/java/com/tesi/java/eventmanager/util/HibernateUtil.java
@@ -5,8 +5,11 @@
  */
 package com.tesi.java.eventmanager.util;
 
-import org.hibernate.cfg.AnnotationConfiguration;
+///import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.cfg.Configuration;
 
 /**
  * Hibernate Utility class with a convenient method to get Session Factory
@@ -17,19 +20,21 @@ import org.hibernate.SessionFactory;
 public class HibernateUtil {
 
     private static final SessionFactory sessionFactory;
-    
+
     static {
         try {
             // Create the SessionFactory from standard (hibernate.cfg.xml) 
             // config file.
-            sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
+            ///sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
+            sessionFactory = new Configuration().configure().buildSessionFactory(
+                    new StandardServiceRegistryBuilder().build());
         } catch (Throwable ex) {
             // Log the exception. 
             System.err.println("Initial SessionFactory creation failed." + ex);
             throw new ExceptionInInitializerError(ex);
         }
     }
-    
+
     public static SessionFactory getSessionFactory() {
         return sessionFactory;
     }
