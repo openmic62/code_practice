6e4905d980da50b09f827bfb5ad7483396ac9e17
mikerocha <mikerocha@mikes-mbp.hsd1.fl.comcast.net>
2016-10-17 18:24:19 -0400

    1.2.1. Mapping the Person class http://docs.jboss.org/hibernate/orm/4.3/manual/en-US/html_single/#tutorial-associations-mappinguser

---
+++ sandbox/EventManager/src/main/java/com/tesi/java/eventmanager/domain/Person.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tesi.java.eventmanager.domain;
+
+/**
+ *
+ * @author mikerocha
+ */
+public class Person {
+    private Long id;
+    private int age;
+    private String firstname;
+    private String lastname;
+    
+    public Person() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    private void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+    
+    
+}
