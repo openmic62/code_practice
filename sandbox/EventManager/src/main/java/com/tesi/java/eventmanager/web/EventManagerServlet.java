9996de7b9e1cd96e7862e9d29bf2f3347d454dc0
mikerocha <mikerocha@mikes-mbp.hsd1.fl.comcast.net>
2016-10-19 21:59:34 -0400

    1.3.1. Writing the basic servlet ... generated new servlet using Netbeans wizard ... implemented doGet() as shown in BV http://docs.jboss.org/hibernate/orm/4.3/manual/en-US/html_single/#tutorial-webapp-servlet

---
+++ sandbox/EventManager/src/main/java/com/tesi/java/eventmanager/web/EventManagerServlet.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tesi.java.eventmanager.web;
+
+import com.tesi.java.eventmanager.util.HibernateUtil;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.rmi.ServerException;
+import java.text.SimpleDateFormat;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author mikerocha
+ */
+public class EventManagerServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet EventManagerServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet EventManagerServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        } finally {
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("dd.MM.yyyy");
+        
+        try {
+            // Begin unit of work
+            HibernateUtil.getSessionFactory().getCurrentSession().getTransaction().begin();
+            
+            // Process request and render page
+            
+            // End unit of work
+            HibernateUtil.getSessionFactory().getCurrentSession().getTransaction().commit();
+        } catch (Exception e) {
+            HibernateUtil.getSessionFactory().getCurrentSession().getTransaction().rollback();
+            if (ServletException.class.isInstance(e)) {
+                throw(ServletException) e;
+            } else {
+                throw new ServletException(e);
+            }
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
