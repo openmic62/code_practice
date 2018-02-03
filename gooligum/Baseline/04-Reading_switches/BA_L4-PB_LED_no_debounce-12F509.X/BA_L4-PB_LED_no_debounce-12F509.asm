326a0a7acf31798d2552d8b9afcf4d951ab0b0da
Mike Rocha <openmic62@gmail.com>
2018-02-03 01:40:18 -0500

    GAS4, p. 13a - MV of code to watch switch for toggling \LED on GP1; code has no SW debounce, so LED toggling is unreliable.

---
+++ gooligum/Baseline/04-Reading_switches/BA_L4-PB_LED_no_debounce-12F509.X/BA_L4-PB_LED_no_debounce-12F509.asm
@@ -0,0 +1,82 @@
+;************************************************************************
+;                                                                       *
+;   Filename:      BA_L4-PB_LED_no_debounce-12F509.asm                  *
+;   Date:          02/02/2018                                           *
+;   File Version:  1.1                                                  *
+;                                                                       *
+;   Author:        Mike Rocha                                           *
+;   Company:       RoCo, Inc.                                           *
+;                                                                       *
+;************************************************************************
+;                                                                       *
+;   Architecture:  Baseline PIC                                         *
+;   Processor:     12F508/509                                           *
+;                                                                       *
+;************************************************************************
+;                                                                       *
+;   Files required: N/A                                                 *
+;                                                                       *
+;************************************************************************
+;                                                                       *
+;   Description:    Lesson 4, p. 13a                                    *
+;                                                                       *
+;   Demonstrates how to watch a switch without debounce                 *
+;                                                                       *
+;                                                                       *
+;************************************************************************
+;                                                                       *
+;   Pin assignments:                                                    *
+;       GP1 = toggling LED                                              *
+;                                                                       *
+;************************************************************************
+
+
+;	list     p=12F509             ; list directive to define processor
+	#include <p12F509.inc>        ; processor specific variable definitions
+	
+;***** CONFIGURATION
+	; ext reset, no code protect, no watchdog, int RC clock
+	__CONFIG   _MCLRE_OFF & _CP_OFF & _WDT_OFF & _IntRC_OSC
+
+; '__CONFIG' directive is used to embed configuration word within .asm file.
+; The lables following the directive are located in the respective .inc file. 
+; See respective data sheet for additional information on configuration word.
+
+;***** VARIABLE DEFINITIONS *****************************************************
+	UDATA_SHR
+sGPIO   res 1                     ; shadow copy of GPIO
+	
+;***** RC CALIBRATION **********************************************************
+RCCAL   CODE    0x3FF             ; processor reset vector
+        res 1                     ; holds internal RC cal value, as a movlw k
+
+;***** RESET VECTOR ************************************************************
+RESET	CODE    0x000             ; effective reset vector
+	movwf   OSCCAL            ; apply internal RC factory calibration 
+		
+;***** MAIN PROGRAM ************************************************************
+	
+;***** Initialization
+start	
+	movlw   b'111101'         ; configure GP1 (only) as an output
+	tris    GPIO
+	
+;***** Main loop
+main_loop
+	; wait for button press
+wait_dn btfsc   GPIO,3
+	goto    wait_dn       
+	
+	; toggle the LED
+	movf    sGPIO,W
+	xorlw   b'00000010'       ; toggle GP1 bit
+	movwf   sGPIO             ; write to shadow
+	movwf   GPIO              ; write to port
+	
+wait_up btfss   GPIO,3            ; if button is up (GP3 high)
+	goto    wait_up
+	
+	; repeat forever
+	goto    main_loop         ; repeat forever
+
+	END
